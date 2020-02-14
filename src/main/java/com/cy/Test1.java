611a9ca7f32423b301942544704fccdb1f65bebe
Asus <Asus@LAPTOP-05SN2DSC>
2020-02-14 20:11:33 +0800

    格式更改

--- src/main/java/com/cy/Test1.java
+++ src/main/java/com/cy/Test1.java
@@ -1,8 +1,10 @@
 package com.cy;
-public class Test1{
-    int age;
-    String name;
-    public static void main(String[] args){
-    System.out.println("模仿别人提交文件");
-    }
+
+public class Test1 {
+	int age;
+	String name;
+
+	public static void main(String[] args) {
+		System.out.println("模仿别人提交文件");
+	}
 }
